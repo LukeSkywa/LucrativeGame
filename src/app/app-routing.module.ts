4f7b872a1554e3b1847f3593800fca2d4dc01edd
davidecarizzoni <davidecarizzoni@gmail.com>
2020-04-17 12:20:45 +0200

    sistemate alcune robe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import { NgModule} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DettaglioComponent } from './components/dettaglio/dettaglio.component';
 import { HomeComponent } from './components/home/home.component';
@@ -8,20 +8,20 @@ import { LoginComponent } from './components/login/login.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes=[
+const routes: Routes = [
+    { path: 'login', component: LoginComponent },
     { path: 'detail/:id', component: DettaglioComponent},
     { path: 'home', component: HomeComponent},
     { path: 'modifica', component: ModificaComponent},
     { path: 'list', component: ListaComponent},
     { path: 'signin', component: SigninComponent},
-    { path: '**', component: PageNotFoundComponent},
-
-    { path: 'login', component: LoginComponent },
+    { path: '**', component:PageNotFoundComponent},
     { path: '', redirectTo: '/login', pathMatch: 'full' }
-];
+]
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
+
 export class AppRoutingModule{}
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
-import { UserItem } from 'src/models/UserInterface';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,36 +10,36 @@ import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
 })
 export class LoginComponent implements OnInit {
 
-  get usernameControl(): FormControl{
-    return this.loginForm.get('username') as FormControl;
-  }
+  // get usernameControl(): FormControl{
+  //   return this.loginForm.get('username') as FormControl;
+  // }
   
-  get passwordControl(): FormControl{
-    return this.loginForm.get('password') as FormControl;
-  }
+  // get passwordControl(): FormControl{
+  //   return this.loginForm.get('password') as FormControl;
+  // }
 
-  get ruoloControl(): FormControl{
-    return this.loginForm.get('ruolo') as FormControl;
-  }
+  // get ruoloControl(): FormControl{
+  //   return this.loginForm.get('ruolo') as FormControl;
+  // }
 
-  rules:string[] = ['admin','user'];
+  // rules:string[] = ['admin','user'];
 
-  loginForm:FormGroup;
+  // loginForm:FormGroup;
 
-  constructor(private loginService:LoginService, private fb:FormBuilder) {
-    this.loginForm=this.fb.group({
-      username: ['', Validators.required],
-      password: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
-      ruolo: ['', Validators.required],
-    });
+  constructor(private loginService:LoginService, private fb:FormBuilder, private  router:Router) {
+    // this.loginForm=this.fb.group({
+    //   username: ['', Validators.required],
+    //   password: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+    //   ruolo: ['', Validators.required],
+    // });
    }
 
   ngOnInit(): void {
   }
 
-  login() {
-      this.loginService.eseguiLogin(this.usernameControl.value,this.passwordControl.value,this.ruoloControl.value);
-  }
+  // login() {
+  //     this.loginService.eseguiLogin(this.usernameControl.value,this.passwordControl.value,this.ruoloControl.value);
+  // }
 
 }
 
