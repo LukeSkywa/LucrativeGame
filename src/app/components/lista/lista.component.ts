c2ba7f28cd5cacdccae00b6cbb02a1c5dad9c5ee
davidecarizzonimatteosorti <matteosorti99.ms@gmail.com>
2020-04-17 11:57:11 +0200

    creata lista clothes

--- src/app/components/lista/lista.component.ts
+++ src/app/components/lista/lista.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ClothesItem } from 'src/models/clothes-item.interface';
+import { ClothesListService } from 'src/app/services/clothes-list.service';
 
 @Component({
   selector: 'app-lista',
@@ -6,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista.component.scss']
 })
 export class ListaComponent implements OnInit {
+  clothesList: ClothesItem[] = [];
 
-  constructor() { }
+  constructor(private clothesListService: ClothesListService) {
+    this.clothesList = this.clothesListService.getClothesList();
+  }
 
   ngOnInit(): void {
   }
