17a51791e3823904403818dd28fe95a4a62c835b
davidecarizzoni <davidecarizzoni@gmail.com>
2020-04-18 11:06:15 +0200

    modificata interfaccia utente, add utenti db.json

--- src/models/user.interface.ts
+++ src/models/user.interface.ts
@@ -1,5 +1,10 @@
-export interface UserItem{
+export interface User{
+    nome: string;
+    cognome: string;
     username: string;
     password: string;
+    email: string;
+    telefono: string
+    genere: string;
     admin: boolean;
 }
