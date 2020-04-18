cc0da5b32f9482e5623982ebe6bad55b92eb5c20
davidecarizzoni <davidecarizzoni@gmail.com>
2020-04-18 09:38:42 +0200

    aggiunto metodo http service

--- src/app/services/http/http.service.ts
+++ src/app/services/http/http.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { ClothesItem } from 'src/models/clothes-item.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,9 @@ export class HttpService {
 
   getClothes(): Observable<any>{
     return this.httpClient.get('http://localhost:3000/clothes');
+  }
+
+  getClothesItem(id: number):Observable<ClothesItem> {
+    return this.httpClient.get<ClothesItem>('http://localhost:3000/clothes?id='+id);
   }
 }
