c98b22158a2ec02abd975a819bff3f0a24ae22df
davidecarizzoni <davidecarizzoni@gmail.com>
2020-04-22 19:25:57 +0200

    aggiornato modifica profilo

--- src/app/components/modifica-profilo/modifica-profilo.component.ts
+++ src/app/components/modifica-profilo/modifica-profilo.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { User } from 'src/models/user.interface';
 import { HttpUserService } from 'src/app/services/http/http-user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-modifica-profilo',
@@ -12,32 +13,33 @@ export class ModificaProfiloComponent implements OnInit {
 
   modificaProfilo: FormGroup;
   user: User;
-  // sesso = ['Uomo', 'Donna', 'Altro'];
+  // generi = ['Uomo', 'Donna', 'Altro'];
 
   // COSTRUISCO IL FORM CON I DATI DELL'UTENTE PRESENTE IN SESSION  
-  constructor(private fb: FormBuilder, private httpUserService: HttpUserService) {
+  constructor(private fb: FormBuilder, private httpUserService: HttpUserService, private router: Router) {
     this.user = JSON.parse(sessionStorage.getItem('user'));
     this.modificaProfilo = this.fb.group({
+      id: this.user.id,
       nome: this.user.nome,
       cognome: this.user.cognome,
+      username: this.user.username,
+      password: this.user.password,
       email: this.user.email,
-      sesso: this.user.genere,
-      telefono: this.user.telefono
+      telefono: this.user.telefono,
+      genere: this.user.genere,
+      admin: this.user.admin,
+      path: this.user.path
+      
     })
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   sendModify(){
-    let userModified: User;
-    userModified = this.modificaProfilo.value;
-    userModified.path = '/assets/users/profile.jpg'
-    userModified.id = this.user.id;
-    console.log(this.modificaProfilo.value)
-    console.log(userModified);
-
-    this.httpUserService.updateUser(userModified).subscribe(()=>{});
+    sessionStorage.setItem('user', JSON.stringify(this.modificaProfilo.value));
+    this.httpUserService.updateUser(this.modificaProfilo.value).subscribe(()=>{});
+    this.router.navigateByUrl('/home');
+    window.alert('Modifica effettuata con sucesso');
   }
 
 }
