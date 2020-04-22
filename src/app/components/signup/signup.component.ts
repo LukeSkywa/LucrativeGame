8b3bb865be748664803f8719effd6959c71697c3
davidecarizzoni <davidecarizzoni@gmail.com>
2020-04-22 17:57:04 +0200

    signup ok

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -23,7 +23,7 @@ export class SignupComponent implements OnInit {
       nome:' -- ',
       cognome: ' -- ',
       email:' -- ',
-      username: ' -- ',
+      username:['', Validators.required],
       password: ['', Validators.required],
       telefono:' -- ',
       genere:' -- ',
