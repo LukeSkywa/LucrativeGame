45561808a55056f70e98600ebd452ee9814cf683
davidecarizzoni <davidecarizzoni@gmail.com>
2020-04-23 15:00:03 +0200

    creata logica cards di base. Aspetto css per implementare tutto

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -10,9 +10,13 @@ import { ClothesItem } from 'src/models/clothes-item.interface';
 })
 export class CardsComponent implements OnInit {
 
-  clothesList: ClothesItem[] = [];
+  mostraPiu: Boolean;
+  clothesList: ClothesItem [] = [];
+  clothesListFiltered: ClothesItem [] = [];
 
-  constructor(private router: Router, private httpClothesService: HttpClothesService) { }
+  constructor(private router: Router, private httpClothesService: HttpClothesService) { 
+    this.mostraPiu = false;
+  }
 
   retrieveClothes(){
     this.httpClothesService.getClothes().subscribe(reponse => {
@@ -21,6 +25,32 @@ export class CardsComponent implements OnInit {
       console.log('errore');
     });
   }
+
+  viewList(list: number){
+    this.clothesListFiltered = this.clothesList.filter( item => {
+      switch(list){
+        case 1: return !item.nascosto; //SE E' NASCOSTO NON LO FACCIO VEDERE. 
+        case 2: return item.preferito;
+        case 3: return item.nascosto;
+      }
+    })
+  }
+
+  switchPreferito(id:number){
+    let clothes: ClothesItem = this.clothesList.find( item => item.id === id);
+    clothes.preferito = !clothes.preferito;
+    this.httpClothesService.updateClothes(clothes).subscribe(() => {});
+  }
+
+  switchNascondi(id:number){
+    let clothes: ClothesItem = this.clothesList.find( item => item.id === id);
+    clothes.nascosto = !clothes.nascosto;
+    this.httpClothesService.updateClothes(clothes).subscribe(() => {});
+  }
+
+  mostraDiPiu(){ this.mostraPiu = true; }
+
+  mostraDiMeno(){ this.mostraPiu = false; }
 
   ngOnInit(): void {
     this.retrieveClothes();
